30d5fa79a7afe284fb8599f2b51a2232e8aba207
tirandasmohith <mohith.tirandas@sanstark.com>
2025-02-10 12:15:41 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 print("hello world")
- print("test scan")
+ print("test scan)
 def f1():
   print("function 1")
 def f2():
   print("function 2")
  def f3():
-  print("code test")
+ print("code test")
